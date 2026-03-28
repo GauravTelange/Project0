24c247bccb83e4cf2c9bcd9a416552008696393e
Gaurav Telange <gauravtelange4321@gmail.com>
2026-03-28 23:08:09 +0530

    add first message

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, Context } = require('telegraf');
 
 const bot = new Telegraf(`8466487482:AAGPPTSAecG7WP6SC99RTiJOPft5awij02g`);
+
+bot.start((ctx) => ctx.reply('Welcome to the new coding bot from Gaurav'));
